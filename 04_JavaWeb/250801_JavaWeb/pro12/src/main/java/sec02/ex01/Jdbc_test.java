72ac94c6395ec6c47e3acb19c7bc74270e2bebf6
kks1177 <kks1177.dev@gmail.com>
2025-08-01 17:11:12 +0900

    250801_JavaWeb-JSP class

---
+++ 04_JavaWeb/250801_JavaWeb/pro12/src/main/java/sec02/ex01/Jdbc_test.java
@@ -0,0 +1,26 @@
+package sec02.ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Jdbc_test {
+
+	public static void main(String[] args) {
+	
+		Connection con = null;
+		try {
+			String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+			String uid = "scott";
+			String pwd = "tiger";
+			String driver = "oracle.jdbc.driver.OracleDriver";
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, uid, pwd);
+			System.out.println("제대로 연결되었습니다.");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("연결에 실패하였습니다.");
+		} // end try
+
+	}// end main
+
+}// end class
